80017b42a515064065f7ff6b336ccdee6fc20d39
silvio-apa <silvio.apanh@gmail.com>
2026-07-04 09:48:42 +0200

    Initialize Spring Boot project

---
+++ src/main/java/ch/wiss/southparkapi/SouthparkApiApplication.java
@@ -0,0 +1,13 @@
+package ch.wiss.southparkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SouthparkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SouthparkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/wiss/southpark_api/SouthparkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.wiss.southpark_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SouthparkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
